139dba56462cfa52efdb7180becc0cc9b2b7b239
brdtrpp <trapp2357@gmail.com>
2015-07-15 12:28:22 -0400

    updated theme and mobile enabled

---
+++ client/templates/classes/classes_list.js
@@ -0,0 +1,46 @@
+var classesData = [
+	{
+		title: 'Introduction Guitar',
+		description: 'Learn the Guitar',
+		location: 'beavercreek',
+		startDate: '7/15/2015',
+		endDate: '7/24/2015',
+		startTime: '11:30',
+		endTime: '1:30',
+		price: '$15/hour'
+	},
+	{
+		title: 'Intermediate Guitar',
+		description: 'Learn the Guitar, this is for the Intermediate students',
+		location: 'Beavercreek',
+		startDate: '7/15/2015',
+		endDate: '7/24/2015',
+		startTime: '11:30',
+		endTime: '1:30',
+		price: '$20/hour'
+	},
+	{
+		title: 'Advanced Guitar',
+		description: 'Learn the Guitar, this is for the most advanced students',
+		location: 'Beavercreek',
+		startDate: '7/15/2015',
+		endDate: '7/24/2015',
+		startTime: '11:30',
+		endTime: '1:30',
+		price: '$25/hour'
+	},
+	{
+		title: 'Another Advanced Guitar',
+		description: 'Learn the Guitar, this is for the most advanced students EVAR!',
+		location: 'beavercreek',
+		startDate: '7/15/2015',
+		endDate: '7/24/2015',
+		startTime: '11:30',
+		endTime: '1:30',
+		price: '$25/hour'
+	},
+];
+
+Template.classesList.helpers({
+	classes: classesData
+})
