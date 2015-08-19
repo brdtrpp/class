e3bbcab71a8321c827f239b99f3e4c0bb53d779f
hexes <trapp2357@gmail.com>
2015-08-19 18:10:23 +0000

    reroute glitch again

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -21,6 +21,11 @@ Template.postSubmit.events({
         // display the error to the user and abort      
         if (error)
             return alert(error.reason);
+            
+        //show this result but route anyway
+        if (postExists)
+            alert('This class has already be submitted');
+            
         Router.go('postPage', {_id: result._id});
     });
   }
