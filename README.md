deecc6d0079bf2a713e6e5573912764016d0dec4
hexes <trapp2357@gmail.com>
2015-08-24 13:37:31 +0000

    added errors

---
+++ README.md
@@ -0,0 +1,31 @@
+#Class
+
+## The Problem
+
+Connecting people with knowledge to share with people who want that knowledge.
+While there is a lot of online solutions for courses, nothing beats an in-person class or course.
+
+As an instructor (be it soccer, coding, math, music etc.) you have to market your classes, get people to register for them, accept their money, track attendance of students and manage your schedule. There are options out there, but the signing on processes is difficult and often you are making more money for the service than yourself. Those options also are only for tutoring, 1-on-1 not a class, bootcamp, course or any other type of instruction learning.
+
+As a student (of any age) if you are interested in learning a new skill, wanting to experience a different world, or just need help with a class. Finding the right instructor can be hard and often . You would ask friends and neighbors for good references, but in the end that can only take you so far. If you wanted to take a class on edible plants in your local area, who would you ask? Dog herding lessons? You would most definitely have to do a lot of research.
+
+Change the way you gain knowledge. 
+
+## The Solution
+
+Class is a marketplace for knowledge, we bring together experts in their field with individuals looking to develop new skills. Whether it be a 1-on-1 math tutoring experience or a 6-month coding bootcamp to change careers.
+
+An instructor can now reach more people in his/her market. The instructor can easily track who has paid, create a schedule for pupils to sign up/register for the class and message the students directly.The marketplace take a small percentage of gross earnings (think elance.com), but in the end the instructor decides what to charge. Established class based businesses as well as freelance instructors can now market their events, camps, courses and classes in an easy to follow standardized way.
+
+Anyone who is looking to learn something new can now hop on the website/app and find a class (in whatever tickles their fancy) that is highly reviewed/rated, join, pay then add that event to their personal calendar without ever having to leave the site/app. The student can decide what kind of class they want (tutoring, small group etc.) and schedule type (weekly, monthly, every other week etc.) and review the instructor's profile to determine the right fit.
+
+Share the world’s knowledge. 
+
+## To Do
+
+* Create Accounts Page
+* Form Validation
+* Comments
+* Notifications
+* Pagenation
+* Voting/Star Stystem
--- client/templates/application/accounts.js
+++ client/templates/application/accounts.js
@@ -1,49 +1,71 @@
 AccountsTemplates.configureRoute('signIn');
 
+AccountsTemplates.addField({
+    _id: 'username',
+    type: 'text',
+    required: true,
+    func: function(value){
+        if (Meteor.isClient) {
+            console.log("Validating username...");
+            var self = this;
+            Meteor.call("userExists", value, function(error, userExists){
+                if (!userExists)
+                    self.setSuccess();
+                else
+                    self.setError(userExists);
+                self.setValidating(false);
+            });
+            return;
+        }
+        // Server
+        return Meteor.call("userExists", value);
+    },
+});
+
 AccountsTemplates.configure({
-    // Behavior
+//     // Behavior
     confirmPassword: true,
     enablePasswordChange: true,
-    forbidClientAccountCreation: false,
-    overrideLoginErrors: true,
-    sendVerificationEmail: true,
-    lowercaseUsername: true,
+//     forbidClientAccountCreation: false,
+//     overrideLoginErrors: true,
+//     sendVerificationEmail: true,
+//     lowercaseUsername: true,
 
-    // Appearance
-    showAddRemoveServices: false,
-    showForgotPasswordLink: true,
-    showLabels: true,
-    showPlaceholders: true,
-    showResendVerificationEmailLink: true,
+//     // Appearance
+    showAddRemoveServices: true,
+//     showForgotPasswordLink: true,
+//     showLabels: true,
+//     showPlaceholders: true,
+//     showResendVerificationEmailLink: true,
 
-    // Client-side Validation
-    continuousValidation: false,
-    negativeFeedback: false,
-    negativeValidation: true,
-    positiveValidation: true,
-    positiveFeedback: true,
-    showValidating: true,
+//     // Client-side Validation
+//     continuousValidation: false,
+//     negativeFeedback: false,
+//     negativeValidation: true,
+//     positiveValidation: true,
+//     positiveFeedback: true,
+//     showValidating: true,
 
-    // Privacy Policy and Terms of Use
-    privacyUrl: 'privacy',
-    termsUrl: 'terms-of-use',
+//     // Privacy Policy and Terms of Use
+//     privacyUrl: 'privacy',
+//     termsUrl: 'terms-of-use',
 
-    // Redirects
-    homeRoutePath: '/',
-    redirectTimeout: 4000,
+//     // Redirects
+//     homeRoutePath: '/',
+//     redirectTimeout: 4000,
 
 
-    // Texts
-    texts: {
-      button: {
-          signUp: "Register Now!"
-      },
-      socialSignUp: "Register",
-      socialIcons: {
-          "meteor-developer": "fa fa-rocket"
-      },
-      title: {
-          forgotPwd: "Recover Your Password"
-      },
-    },
+//     // Texts
+//     texts: {
+//       button: {
+//           signUp: "Register Now!"
+//       },
+//       socialSignUp: "Register",
+//       socialIcons: {
+//           "meteor-developer": "fa fa-rocket"
+//       },
+//       title: {
+//           forgotPwd: "Recover Your Password"
+//       },
+//     },
 });
